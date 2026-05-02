339092a6752791be68cd411e9c6b8fb6298d2039
Igor <waszczeniukigor@gmail.com>
2026-05-02 23:00:15 +0200

    lab_e

---
+++ lab-e/src/script.ts
@@ -0,0 +1,43 @@
+const styles: Record<string, string> = {
+    style1: "/style-1.css",
+    style2: "/style-2.css"
+};
+
+let currentStyle: string = "style1";
+
+function applyStyle(styleKey: string) {
+    const oldLink = document.getElementById("dynamic-style");
+    if (oldLink) {
+        oldLink.remove();
+    }
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = styles[styleKey];
+    link.id = "dynamic-style";
+    document.head.appendChild(link);
+    currentStyle = styleKey;
+}
+
+function createStyleButtons() {
+    const container = document.createElement("div");
+    //styluje juz tutaj, mialem problem bo mi jakis element zasłaniał "przeklikiwanie" w stylu 2 i musialem
+    //w taki sposob to obejsc po prostu
+    container.style.position = "fixed";
+    container.style.top = "10px";
+    container.style.left = "10px";
+
+    Object.keys(styles).forEach(style => {
+        const button = document.createElement("button");
+        button.textContent = style;
+        button.style.marginRight = "5px";
+        button.style.cursor = "pointer";
+        button.onclick = () => applyStyle(style);
+        container.appendChild(button);
+    });
+    document.body.appendChild(container);
+}
+
+window.onload = () => {
+    applyStyle(currentStyle);
+    createStyleButtons();
+};
