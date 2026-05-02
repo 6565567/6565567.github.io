22ae1572e8973c4c4f31c063220556f00c8d360b
Igor <waszczeniukigor@gmail.com>
2026-05-02 23:32:21 +0200

    lab_e

--- lab-e/src/script.ts
+++ lab-e/src/script.ts
@@ -1,6 +1,7 @@
 const styles: Record<string, string> = {
-    style1: "public/style-1.css",
-    style2: "public/style-2.css"
+    style1: "/style-1.css",
+    style2: "/style-2.css",
+    style3: "/style-3.css"
 };
 
 let currentStyle: string = "style1";
